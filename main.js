b85e6d28b5a272057d78c19303451c5b97fa2dde
flexult <touma.leandre.72@gmail.com>
2026-01-18 20:32:22 +0000

    !!! Espace de travail vide. Pret à l'emploi

--- main.js
+++ main.js
@@ -26,58 +26,6 @@ const game = new Phaser.Game(config);
 
 function preload () {}
 
-function create () {
-    const jauge = new Jauge(
-        this, 
-        {x: 200, y: 200}, 
-        {min: 0, max: 5},
-        {
-            length: 300,
-            color: 0x333333,
-            isVertical: true
-        }
-    );
+function create () {}
 
-    const dice = new Dice(this, 70, 70);
-
-    const toggleSquare = new ToggleSquare(
-        this,
-        {x: 300, y: 160},
-        {
-            initialState: true,
-            size: 65
-        }
-    );
-
-    const deck = new CardDeck(
-        this, 
-        {x: 340, y: 300},
-        "TECHNOLOGIES",
-        ["CARTE 1", " NO 2", "La 3 stp", "4 ?", "Voila: 5"],
-        (chosenCard) => {
-            alert('carte choisie: ' + chosenCard);
-        },
-        false
-    );
-
-    const otherDeck = new CardDeck(
-        this,
-        {x: 360, y: 420},
-        "WIN !!",
-        ["win1", "win2", "win3", "win4", "win5", "win6", "win7"],
-        (chosenCard) => {
-            alert("Carte choisie = " + chosenCard);
-        }
-    )
-
-    const counter = new Counter(
-        this,
-        {x: 400, y: 550},
-        "GOLD",
-        2,
-        [0]
-    );
-}
-
-function update () {
-}
+function update () {}
