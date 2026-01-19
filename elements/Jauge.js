97a60ab9a374f97607b36ba5140e43bf747833fe
flexult <touma.leandre.72@gmail.com>
2026-01-19 19:31:19 +0000

    retour en arriere

--- elements/Jauge.js
+++ elements/Jauge.js
@@ -1,9 +1,5 @@
-export class Jauge extends Phaser.GameObjects.DOM {
+export class Jauge {
   constructor(scene, coos, bornes, params={}) { // params can contain color, isVertical and length
-    const el = document.createElement('div');
-    super(scene, coos.x, coos.y, el);
-    scene.add.existing(this);
-
     this.scene = scene;
     this.x = coos.x;
     this.y = coos.y;
