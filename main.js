b9458b371f41401be1558e0ee81036cf50c62867
flexult <touma.leandre.72@gmail.com>
2026-01-19 19:24:27 +0000

    dom

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ const config = {
     width: window.innerWidth,
     height: window.innerHeight,
     parent: gameContainer,
+    dom: {
+        createContainer: true
+    },
     scale: {
         mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH
